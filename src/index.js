601f0f3add344b676d3c226f92e16888d11554ef
dougglas1 <dougglas1994@hotmail.com>
2020-01-17 19:01:31 -0300

    Primeiro Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
+// Necessário importar o React quando utilizar HTML no JavaScript (JSX)
 import React from 'react';
+
+// Permite comuniar o React com HTML (Árvore de Elementos)
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+// Renderizar o conteúdo do App.js dentro do ID root do HTML
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
